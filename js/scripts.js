6c932a4e7c2913060dabf69de166e6c4ca2ff9de
faizanakram99 <faizanakram99@gmail.com>
2016-02-20 18:02:20 +0530

    changed layout

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        menu: '#mobile-dropdown',
+        sectionsColor: ['#006495', '#F2635F', '#A2E9EC', '#F4D00C'],
+        anchors:['home', 'portfolio', 'about','contact'],
+        css3: true
+    });
+
+
+    $("#mobile-dropdown ul li a").on("click", function () {
+        $("#mobile-dropdown ul li a").removeClass("active");
+        $(this).addClass("active");
+    });
+
+
+});
+
+
