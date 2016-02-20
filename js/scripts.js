06b646a3b8bf8b61d563def336feb265e0fd8648
Faizan Akram <faizanakram99@users.noreply.github.com>
2016-02-20 18:01:16 +0530

    Added files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,19 @@
+
 $(document).ready(function () {
-    $(".main-nav li a").on("click", function () {
-        $(".main-nav li a").removeClass("active");
+    $('#fullpage').fullpage({
+        menu: '#mobile-dropdown',
+        sectionsColor: ['#006495', '#F2635F', '#A2E9EC', '#F4D00C'],
+        anchors:['home', 'portfolio', 'about','contact'],
+        css3: true
+    });
+    
+        
+    $("#mobile-dropdown ul li a").on("click", function () {
+        $("#mobile-dropdown ul li a").removeClass("active");
         $(this).addClass("active");
-    });
-
-    $("#mobile-nav").on("click", function () {
-        $("#nav-menu").slideToggle();
-    });
-
-
+    }); 
+    
+      
 });
 
-
-
+ 
