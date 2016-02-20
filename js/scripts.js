b2accd1a4de63b0edbb918f600c9c84422c59792
faizanakram99 <faizanakram99@gmail.com>
2016-02-20 18:14:09 +0530

    deleted unwanted files

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 $(document).ready(function () {
     $('#fullpage').fullpage({
         menu: '#mobile-dropdown',
@@ -16,26 +14,3 @@ $(document).ready(function () {
 
 
 });
-
-
-=======
-
-$(document).ready(function () {
-    $('#fullpage').fullpage({
-        menu: '#mobile-dropdown',
-        sectionsColor: ['#006495', '#F2635F', '#A2E9EC', '#F4D00C'],
-        anchors:['home', 'portfolio', 'about','contact'],
-        css3: true
-    });
-
-
-    $("#mobile-dropdown ul li a").on("click", function () {
-        $("#mobile-dropdown ul li a").removeClass("active");
-        $(this).addClass("active");
-    });
-
-
-});
-
-
->>>>>>> faizanakram99.github.io/master
