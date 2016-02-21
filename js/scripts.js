3cdb3f10228dcbd3ec87c5a31ee05da89cfdfa7d
faizanakram99 <faizanakram99@gmail.com>
2016-02-21 06:28:43 +0530

    added contact page halt

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('#fullpage').fullpage({
         menu: '#mobile-dropdown',
-        sectionsColor: ['#006495', '#F2635F', '#A2E9EC', '#F4D00C'],
+        sectionsColor: ['#006495', '#F2635F', '#333', '#39a4aa'],
         anchors: ['home', 'portfolio', 'about', 'contact'],
         paddingTop: '80px',
         paddingBottom: '30px',
