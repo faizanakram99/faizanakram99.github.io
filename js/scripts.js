24bc9b0e31ceebf8d9b886248d8ed8b0b20bcae0
faizanakram99 <faizanakram99@gmail.com>
2016-02-22 00:03:20 +0530

    Added autoslider!

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,31 @@
 $(document).ready(function () {
-    $('#fullpage').fullpage({
-        menu: '#mobile-dropdown',
-        sectionsColor: ['#006495', '#F2635F', '#333', '#39a4aa'],
-        anchors: ['home', 'portfolio', 'about', 'contact'],
-        paddingTop: '80px',
-        paddingBottom: '30px',
-    });
+    var slideTimeout;
+            $('#fullpage').fullpage({
+                    menu: '#mobile-dropdown',
+                    sectionsColor: ['#006495', '#F2635F', '#333', '#39a4aa'],
+                    anchors: ['home', 'portfolio', 'about', 'contact'],
+                    paddingTop: '80px',
+                    paddingBottom: '30px',
+                    afterRender: function () {
+                        //on page load, start the slideshow
+                        slideTimeout = setInterval(function () {
+                            $.fn.fullpage.moveSlideRight();
+                        }, 2700);
+                    },
 
-    if ($(window).width() < 800){
-        $.fn.fullpage.setAutoScrolling(false);
-    }
+
+                    onLeave: function (index, direction) {
+                        //after leaving section 1 (home) and going anywhere else, whether scrolling
+                    }
+            });
+
+                if ($(window).width() < 800) {
+                    $.fn.fullpage.setAutoScrolling(false);
+                }
 
 
-    $("#mobile-dropdown ul li a").on("click", function () {
-        $("#mobile-dropdown ul li a").removeClass("active");
-        $(this).addClass("active");
-    });
-});
-
+                $("#mobile-dropdown ul li a").on("click", function () {
+                    $("#mobile-dropdown ul li a").removeClass("active");
+                    $(this).addClass("active");
+                });
+            });
