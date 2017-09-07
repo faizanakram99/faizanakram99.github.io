d9d4eb9ab3a687d0a190aa5aee356a1e102380df
Faizan Akram <faizanakram99@gmail.com>
2017-09-07 23:40:35 +0530

    some style changes and minor code improvement

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,45 @@
-$(document).ready(function () {
+$(function () {
+    const $window = $(window);
+    const $document = $(document);
+    const $fullpage = $document.find("#fullpage");
+    const $navbar = $document.find("#mobile-dropdown");    
+    const $menuitem = $navbar.find("ul li a");
+    
     var slideTimeout;
-            $('#fullpage').fullpage({
-                    menu: '#mobile-dropdown',
-                    sectionsColor: ['#006495', '#F2635F', '#333', '#39a4aa'],
-                    anchors: ['home', 'portfolio', 'about', 'contact'],
-                    paddingTop: '80px',
-                    paddingBottom: '30px',
-                    afterRender: function () {
-                        //on page load, start the slideshow
-                        slideTimeout = setInterval(function () {
-                            $.fn.fullpage.moveSlideRight();
-                        }, 2700);
-                    },
 
+    $fullpage
+        .fullpage({
+            menu: '#mobile-dropdown',
+            sectionsColor: ['#006495', '#F2635F', '#333', '#39a4aa'],
+            anchors: ['home', 'portfolio', 'about', 'contact'],
+            paddingTop: '80px',
+            paddingBottom: '30px',
+            afterRender: function () {
+                //on page load, start the slideshow
+                slideTimeout = setInterval(function () {
+                    $.fn.fullpage.moveSlideRight();
+                }, 2700);
+            }
+        })
+    ;
 
-                    onLeave: function (index, direction) {
-                        //after leaving section 1 (home) and going anywhere else, whether scrolling
-                    }
-            });
+    if ($window.width() < 800) {
+        $.fn.fullpage.setAutoScrolling(false);
+    }
 
-                if ($(window).width() < 800) {
-                    $.fn.fullpage.setAutoScrolling(false);
-                }
+    const makeItemActive = function () {
+        $menuitem.removeClass("active");
+        $(this).addClass("active");
+    };
 
+    const closeNavbar = function (event) {
+        const clickover = $(event.target);
+        if ($navbar.hasClass("in") && !clickover.hasClass("navbar-toggle")) {      
+            $navbar.collapse('hide');
+        }
+    }
 
-                $("#mobile-dropdown ul li a").on("click", function () {
-                    $("#mobile-dropdown ul li a").removeClass("active");
-                    $(this).addClass("active");
-                });
-            });
+    $menuitem.on("click", makeItemActive);
+    $document.on("click", closeNavbar);
+
+});
