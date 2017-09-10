203db4c04349b3b0613ccc769e36fbed4bc2e4df
Faizan Akram <faizanakram99@gmail.com>
2017-09-10 15:48:55 +0530

    improved code smell

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,65 @@
 $(function () {
-    const $window = $(window);
-    const $document = $(document);
-    const $fullpage = $document.find("#fullpage");
-    const $navbar = $document.find("#mobile-dropdown");    
-    const $menuitem = $navbar.find("ul li a");
+     /**
+     * @type {Element}
+     * Cache DOM
+     */
+    const $window = $(window),
+          $document = $(document),
+          $fullpage = $document.find("#fullpage"),
+          $navbar = $document.find("#mobile-dropdown"),
+          $menuitem = $navbar.find("ul li a");
     
-    var slideTimeout;
+    /** @function */
+    let slideTimeout;
 
-    $fullpage
-        .fullpage({
-            menu: '#mobile-dropdown',
-            sectionsColor: ['#006495', '#F2635F', '#333', '#39a4aa'],
-            anchors: ['home', 'portfolio', 'about', 'contact'],
-            paddingTop: '80px',
-            paddingBottom: '30px',
-            afterRender: function () {
-                //on page load, start the slideshow
-                slideTimeout = setInterval(function () {
-                    $.fn.fullpage.moveSlideRight();
-                }, 2700);
-            }
-        })
-    ;
+    /** @function */    
+    const afterRender = function () {
+        //on page load, start the slideshow
+        slideTimeout = setInterval(function () {
+            $.fn.fullpage.moveSlideRight();
+        }, 2700);
+    };
 
-    if ($window.width() < 800) {
-        $.fn.fullpage.setAutoScrolling(false);
-    }
+    /** 
+     * @type {Object} 
+     * Object containing options to be passed fullpage() method
+     */
+    const fullpageOptions = {
+        menu: '#mobile-dropdown',
+        sectionsColor: ['#006495', '#F2635F', '#333', '#39a4aa'],
+        anchors: ['home', 'portfolio', 'about', 'contact'],
+        paddingTop: '80px',
+        paddingBottom: '30px',
+        afterRender: afterRender
+    };
 
+    /** 
+     * @function
+     * Click Event Listener of $menuitem,
+     * makes clicked link active 
+     */
     const makeItemActive = function () {
         $menuitem.removeClass("active");
         $(this).addClass("active");
     };
 
+    /** 
+     * @function
+     * Click Event Listener of $document,
+     * closes navbar
+     */
     const closeNavbar = function (event) {
         const clickover = $(event.target);
         if ($navbar.hasClass("in") && !clickover.hasClass("navbar-toggle")) {      
             $navbar.collapse('hide');
         }
-    }
+    }    
 
+    /**
+     * Bind Event Listeners
+     */
     $menuitem.on("click", makeItemActive);
     $document.on("click", closeNavbar);
-
+    $fullpage.fullpage(fullpageOptions);    
+    if($window.width() < 800) $.fn.fullpage.setAutoScrolling(false);
 });
